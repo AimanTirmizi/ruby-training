e10c9b8595c9790ac2db678d6aac9be7e1711c4f
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-02-25 19:50:30 +0000

    Add test to validate non-integers

--- validators/lib/number_validator.rb
+++ validators/lib/number_validator.rb
@@ -2,6 +2,10 @@
 
 class NumberValidator
   def validate(number)
-    number % 2 == 0 ? [:even] : [:odd]
+    if number.is_a?(String)
+      raise ArgumentError
+    else
+      number % 2 == 0 ? [:even] : [:odd]
     end
+  end
 end
