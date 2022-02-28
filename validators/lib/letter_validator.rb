17ddb2635a20d9f0e91927fde1b510893b2c64f8
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-02-28 17:07:25 +0000

    Updated test to validate all consonants and vowels

--- validators/lib/letter_validator.rb
+++ validators/lib/letter_validator.rb
@@ -3,10 +3,9 @@
 class LetterValidator
   def validate(letter)
     raise InvalidLetterError if letter.length > 1
-    return [:vowel] if letter.match(/[aeiou+]/)
-    return [:consonant] if letter.match(/[^aeiou+]/)
+    letter.match(/[aeiou]/) ? [:vowel] : [:consonant]
     end
-  end
+end
 
 # Do not edit this code!
 class InvalidLetterError < ArgumentError; end
