468fc37c15220219e5c180f09bfc1023729d08f3
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-03-01 15:58:17 +0000

    Add test methods to validate all conditions

--- validators/lib/letter_validator.rb
+++ validators/lib/letter_validator.rb
@@ -3,8 +3,21 @@
 class LetterValidator
   def validate(letter)
     raise InvalidLetterError if letter.length > 1
-    letter.match(/[aeiou]/) ? [:vowel] : [:consonant]
-    end
+  end
+
+  def validate_type(letter)
+    return [:vowel] if letter.match(/[aeiou]/)
+    [:consonant]
+  end
+
+  def validate_half(letter)
+    return [:first_half] if letter.match(/[a-m]/)
+    [:second_half]
+  end
+
+  def validate_usage(letter)
+    return [:common_letter] if letter.match(/[etaio]/)
+  end
 end
 
 # Do not edit this code!
