875d45c8cc0402e16675357c0f00aaa9cb89cfdb
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-03-01 16:54:57 +0000

    Add private test methods to validate all conditions

--- validators/lib/number_validator.rb
+++ validators/lib/number_validator.rb
@@ -2,10 +2,30 @@
 
 class NumberValidator
   def validate(number)
-    if number.is_a?(String)
-      raise ArgumentError
-    else
-      number % 2 == 0 ? [:even] : [:odd]
-    end
+    raise ArgumentError if number.is_a?(String)
+    obj = NumberValidator.new
+    obj.validate_type(number)
   end
+
+  def validate_type(number)
+    return [:even] if number % 2 == 0
+    [:odd]
+  end
+
+  def multiples_of_three(number)
+    return [:divisible_by_three] if number % 3 == 0
+  end
+
+  def multiples_of_five(number)
+    return [:divisible_by_five] if number % 5 == 0
+  end
+
+  def multiples_of_seven(number)
+    return [:divisible_by_seven] if number % 7 == 0
+  end
+
+  def multiples_of_nine(number)
+    return [:divisible_by_nine] if number % 9 == 0
+  end
+
 end
