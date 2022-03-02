769e90fed57327214bc7adb77c737511714322e6
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-03-02 14:55:16 +0000

    Add attr_reader and initialize instance variable with an empty array and modified test methods to return symbols instead of an array

--- validators/lib/number_validator.rb
+++ validators/lib/number_validator.rb
@@ -1,31 +1,35 @@
 # frozen_string_literal: true
 
 class NumberValidator
+  attr_reader :values
+
+  def initialize
+    @values = []
+  end
+
   def validate(number)
     raise ArgumentError if number.is_a?(String)
-    obj = NumberValidator.new
-    obj.validate_type(number)
   end
 
   def validate_type(number)
-    return [:even] if number % 2 == 0
-    [:odd]
+    return :even if number % 2 == 0
+    :odd
   end
 
   def multiples_of_three(number)
-    return [:divisible_by_three] if number % 3 == 0
+    return :divisible_by_three if number % 3 == 0
   end
 
   def multiples_of_five(number)
-    return [:divisible_by_five] if number % 5 == 0
+    return :divisible_by_five if number % 5 == 0
   end
 
   def multiples_of_seven(number)
-    return [:divisible_by_seven] if number % 7 == 0
+    return :divisible_by_seven if number % 7 == 0
   end
 
   def multiples_of_nine(number)
-    return [:divisible_by_nine] if number % 9 == 0
+    return :divisible_by_nine if number % 9 == 0
   end
 
 end
