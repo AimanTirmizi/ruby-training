ad752992dd4549cb7710dfc52df645e733fb7dd0
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-03-04 15:45:59 +0000

    Modified optional args method verses(*bottles) to verses(first_verse, second_verse) & rename argument bottles to first_verse in verse

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class Bottles
-  def verse(bottle)
+  def verse(first_verse)
     "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
   end
 
-  def verses(*bottles)
+  def verses(first_verse, last_verse)
     "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n\n"
   end
 end
