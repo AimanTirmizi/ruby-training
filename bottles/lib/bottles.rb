caa0a72724a2c7ee5dd1efc89a2f009b64262e1c
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-03-04 12:28:54 +0000

    Add method verses(*bottles)

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -4,4 +4,8 @@ class Bottles
   def verse(bottle)
     "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
   end
+
+  def verses(*bottles)
+    "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n\n"
+  end
 end
