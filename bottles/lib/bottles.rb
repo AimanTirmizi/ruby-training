addc22a1f85bd84e3e9163c3bd78583f8890a5e1
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-03-08 12:34:25 +0000

    Add method number_of_bottles(number) to interpolate bottles values

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,10 +2,15 @@
 
 class Bottles
   def verse(first_verse)
-    "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n"
+    number_of_bottles(first_verse)
   end
 
   def verses(first_verse, last_verse)
     "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n\n"
+  end
+
+  def number_of_bottles(number)
+      bottlesOnWall = "bottles of beer on the wall"
+      "#{number} #{bottlesOnWall}, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} #{bottlesOnWall}.\n"
   end
 end
