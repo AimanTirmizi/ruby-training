ba00ca5eb726b42f3e1760ae2f342eecb2fd65c2
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-03-09 16:55:00 +0000

    Updated strings styling in number_of_bottles(number)

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -15,13 +15,25 @@ class Bottles
 
   def number_of_bottles(number)
     if number == 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, #{number + 99} bottles of beer on the wall.\n"
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
     elsif number == 1
-      "#{number} bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
     elsif number == 2
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottle of beer on the wall.\n"
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, "+
+      "#{number - 1} bottles of beer on the wall.\n"
     end
   end
 end
