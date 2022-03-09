d6d95a1614e20f4611985a9d888e176bc0f5ce56
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-03-09 15:07:50 +0000

    Add condition if number of bottles are 2

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -14,6 +14,8 @@ class Bottles
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, #{number + 99} bottles of beer on the wall.\n"
     elsif number == 1
       "#{number} bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    elsif number == 2
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottle of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
     end
