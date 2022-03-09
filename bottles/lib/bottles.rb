9dbaf454f372b30f40fd48e8d2638dd0ce65606d
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-03-09 16:15:56 +0000

    Add method 'sing' for the whole song

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -9,6 +9,10 @@ class Bottles
     "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n\n"
   end
 
+  def sing
+    verses(99,0)
+  end
+
   def number_of_bottles(number)
     if number == 0
       "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, #{number + 99} bottles of beer on the wall.\n"
