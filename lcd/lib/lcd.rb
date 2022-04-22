5389fc9c46f91681c92ba654340470ba16eabc42
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-04-22 21:55:04 +0100

    Refactored render method to render 487

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -15,6 +15,8 @@ class LCD
       matrixDisplay[0].join
     when 6
       matrixDisplay[6].join
+    when 487
+      matrixDisplay[4].join + matrixDisplay[8].join + matrixDisplay[7].join
     end
   end
 
@@ -40,7 +42,7 @@ class LCD
       1 => [newline, space, space, pipe, newline, space, space, pipe],
       2 => [space, underscore, newline, space, underscore, pipe, newline, pipe, underscore],
       3 => [underscore, space, newline, underscore, pipe, newline, underscore, pipe],
-      4 => [pipe, underscore, pipe, newline, space, space, pipe],
+      4 => [pipe, underscore, pipe, newline, space, space, pipe, newline],
       5 => [space, underscore, space, newline, pipe, underscore, newline, space, underscore, pipe],
       6 => [space, underscore, space, newline, pipe, underscore, space, newline, pipe, underscore, pipe, newline],
       7 => [space, underscore, space, newline, space, space, pipe, newline, space, space, pipe],
