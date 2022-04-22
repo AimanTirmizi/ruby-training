62087b6481edca3ba2f768b674b365f1514d36ca
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-04-22 17:42:17 +0100

    Refactored hash to render all values

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -36,9 +36,22 @@ class LCD
 
   def matrixDisplay
     {
-      8 => [" _ ", "\n", "|", "_", "|", "\n", "|", "_", "|", "\n"],
-      6 => [" _ ", "\n", "|", "_", " ", "\n", "|", "_", "|", "\n"],
-      0 => [" _ ", "\n", "|", " ", "|", "\n", "|", "_", "|", "\n"]
+      0 => [space, underscore, space, newline, pipe, space, pipe, newline, pipe, underscore, pipe, newline],
+      1 => [newline, space, space, pipe, newline, space, space, pipe],
+      2 => [space, underscore, newline, space, underscore, pipe, newline, pipe, underscore],
+      3 => [underscore, space, newline, underscore, pipe, newline, underscore, pipe],
+      4 => [pipe, underscore, pipe, newline, space, space, pipe],
+      5 => [space, underscore, space, newline, pipe, underscore, newline, space, underscore, pipe],
+      6 => [space, underscore, space, newline, pipe, underscore, space, newline, pipe, underscore, pipe, newline],
+      7 => [space, underscore, space, newline, space, space, pipe, newline, space, space, pipe],
+      8 => [space, underscore, space, newline, pipe, underscore, pipe, newline, pipe, underscore, pipe, newline],
+      9 => [space, underscore, space, newline, pipe, underscore, pipe, newline, space, underscore, pipe],
+      'a' => [space, underscore, space, newline, pipe, underscore, pipe, newline, pipe, space, pipe, newline],
+      'b' => [pipe, underscore, space, newline, pipe, underscore, pipe, newline],
+      'c' => [space, underscore, space, newline, pipe, space, newline, pipe, underscore, space, newline],
+      'd' => [space, underscore, pipe, newline, pipe, underscore, pipe, newline],
+      'e' => [space, underscore, space, newline, pipe, underscore, newline, pipe, underscore],
+      'f' => [space, underscore, space, newline, pipe, underscore, newline, pipe]
     }
   end
 end
