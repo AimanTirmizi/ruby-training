1aefb2bb7bef8175cdb90a839602618b6605c63d
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-04-29 16:52:10 +0100

    Refactored code to render all numbers

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -12,11 +12,11 @@ class LCD
   end
 
   def space
-    ' '
+    " "
   end
 
   def underscore
-    '_'
+    "_"
   end
 
   def newline
@@ -24,26 +24,32 @@ class LCD
   end
 
   def pipe
-    '|'
+    "|"
   end
 
   def render_lcd(items)
     # convert string or numbers into array & iterate through it
-    matrixDisplay[items]
+    array = []
+    if items.is_a?(Integer)
+      items.digits.reverse.each { |lcd|
+        array << matrixDisplay[lcd]
+      }
+      array
+    end
   end
 
   def matrixDisplay
     {
       0 => [space, underscore, space, newline, pipe, space, pipe, newline, pipe, underscore, pipe, newline],
-      1 => [newline, space, space, pipe, newline, space, space, pipe],
+      1 => [newline, space, space, pipe, newline, space, space, pipe, newline],
       2 => [space, underscore, newline, space, underscore, pipe, newline, pipe, underscore],
-      3 => [underscore, space, newline, underscore, pipe, newline, underscore, pipe],
+      3 => [underscore, space, newline, underscore, pipe, newline, underscore, pipe, newline],
       4 => [pipe, underscore, pipe, newline, space, space, pipe, newline],
-      5 => [space, underscore, space, newline, pipe, underscore, newline, space, underscore, pipe],
+      5 => [space, underscore, space, newline, pipe, underscore, newline, space, underscore, pipe, newline],
       6 => [space, underscore, space, newline, pipe, underscore, space, newline, pipe, underscore, pipe, newline],
-      7 => [space, underscore, space, newline, space, space, pipe, newline, space, space, pipe],
+      7 => [space, underscore, space, newline, space, space, pipe, newline, space, space, pipe, newline],
       8 => [space, underscore, space, newline, pipe, underscore, pipe, newline, pipe, underscore, pipe, newline],
-      9 => [space, underscore, space, newline, pipe, underscore, pipe, newline, space, underscore, pipe],
+      9 => [space, underscore, space, newline, pipe, underscore, pipe, newline, space, underscore, pipe, newline],
       'a' => [space, underscore, space, newline, pipe, underscore, pipe, newline, pipe, space, pipe, newline],
       'b' => [pipe, underscore, space, newline, pipe, underscore, pipe, newline],
       'c' => [space, underscore, space, newline, pipe, space, newline, pipe, underscore, space, newline],
