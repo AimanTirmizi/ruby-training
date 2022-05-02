f2de1bff8f486b865b18766ecc1f9e981119e85b
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-05-02 23:58:06 +0100

    Refactored code to render all numbers and letters at one line

--- lcd/lib/lcd.rb
+++ lcd/lib/lcd.rb
@@ -35,27 +35,50 @@ class LCD
     else
       items.chars.each { |lcd| array << matrix_display[lcd] }
     end
-    array.join
+    render_num_letters(array)
   end
 
   def matrix_display
     {
       0 => [space, underscore, space, newline, pipe, space, pipe, newline, pipe, underscore, pipe, newline],
-      1 => [newline, space, space, pipe, newline, space, space, pipe, newline],
-      2 => [space, underscore, newline, space, underscore, pipe, newline, pipe, underscore, newline],
-      3 => [underscore, space, newline, underscore, pipe, newline, underscore, pipe, newline],
-      4 => [pipe, underscore, pipe, newline, space, space, pipe, newline],
-      5 => [space, underscore, space, newline, pipe, underscore, newline, space, underscore, pipe, newline],
+      1 => [space, space, space, newline, space, space, pipe, newline, space, space, pipe, newline],
+      2 => [space, underscore,space, newline, space, underscore, pipe, newline, pipe, underscore, space, newline],
+      3 => [space, underscore, space, newline, space, underscore, pipe, newline, space, underscore, pipe, newline],
+      4 => [space, space, space, newline, pipe, underscore, pipe, newline, space, space, pipe, newline],
+      5 => [space, underscore, space, newline, pipe, underscore, space, newline, space, underscore, pipe, newline, newline],
       6 => [space, underscore, space, newline, pipe, underscore, space, newline, pipe, underscore, pipe, newline],
       7 => [space, underscore, space, newline, space, space, pipe, newline, space, space, pipe, newline],
       8 => [space, underscore, space, newline, pipe, underscore, pipe, newline, pipe, underscore, pipe, newline],
       9 => [space, underscore, space, newline, pipe, underscore, pipe, newline, space, underscore, pipe, newline],
       'a' => [space, underscore, space, newline, pipe, underscore, pipe, newline, pipe, space, pipe, newline],
-      'b' => [pipe, underscore, space, newline, pipe, underscore, pipe, newline],
-      'c' => [space, underscore, space, newline, pipe, space, newline, pipe, underscore, space, newline],
-      'd' => [space, underscore, pipe, newline, pipe, underscore, pipe, newline],
-      'e' => [space, underscore, space, newline, pipe, underscore, newline, pipe, newline],
-      'f' => [space, underscore, space, newline, pipe, underscore, newline, pipe, newline]
+      'b' => [space, space, space, newline, pipe, underscore, space, newline, pipe, underscore, pipe, newline],
+      'c' => [space, underscore, space, newline, pipe, space, space, newline, pipe, underscore, space, newline],
+      'd' => [space, space, space, newline, space, underscore, pipe, newline, pipe, underscore, pipe, newline],
+      'e' => [space, underscore, space, newline, pipe, underscore, space, newline, pipe, underscore, space, newline],
+      'f' => [space, underscore, space, newline, pipe, underscore,space, newline, pipe,space, space, newline]
     }
+  end
+
+  private
+
+  def render_num_letters(array)
+    arr1 = []
+    arr2 = []
+    arr3 = []
+    array.map.each { |item|
+      new_data = 0
+      item.map.each { |option|
+        if option != newline && new_data.zero?
+          arr1 << option
+        elsif option != newline && new_data == 1
+          arr2 << option
+        elsif option != newline && new_data == 2
+          arr3 << option
+        elsif option == newline
+          new_data += 1
+        end
+      }
+    }
+    (arr1.push(newline) + arr2.push(newline) + arr3.push(newline)).join
   end
 end
