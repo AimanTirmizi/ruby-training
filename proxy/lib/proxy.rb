106adbd2941cd30b5706ce76d977f62d66824149
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-05-05 16:24:09 +0100

    Add handler method missing to catch channel= method

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -6,4 +6,8 @@ class Proxy
   def initialize(proxy_object)
     @proxy_object = proxy_object
   end
+
+  def method_missing(method_name, *args, &block)
+    proxy_object.send(method_name, *args, &block)
+  end
 end
