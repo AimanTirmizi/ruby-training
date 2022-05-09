f9737a9f5b470db3edbcac59bb8bf688b624aaea
Aiman Syed <12950084+AimanTirmizi@users.noreply.github.com>
2022-05-09 17:30:50 +0100

    Add TDD tests for Proxy (#46)
    * Initialize proxy_object
    
    * Add handler method missing to catch channel= method
    
    * Add method messages to send strings
    
    * Add attr_reader messages and initialized it with an empty array
    
    * Add method number_of_times_called to count the methods being called
    
    * Add method called? to ensure proxy methods being called
    
    * Add method on? for the proxy objects methods
    
    * Refactored code to add respond_to? method to respond for on? method
    
    * Refactored code to add method to call super method respond_to? to respond for called? method of proxy

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
 
 class Proxy
+  attr_reader :proxy_object, :messages
+
+  def initialize(proxy_object)
+    @proxy_object = proxy_object
+    @messages = []
+  end
+
+  def method_missing(method_name, *args, &block)
+    messages << method_name
+    proxy_object.send(method_name, *args, &block)
+  end
+
+  def number_of_times_called(method_name)
+    messages.count(method_name)
+  end
+
+  def called?(method_name)
+    messages.include?(method_name)
+  end
+
+  def respond_to?(method_name)
+    proxy_object.respond_to?(method_name) || super(method_name)
+  end
 end
