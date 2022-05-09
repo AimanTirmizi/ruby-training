2cd0fd4ab49d133149d7e4e30a2abbe3cfd8c17b
aimantirmizi <syedatirmiziaiman@gmail.com>
2022-05-09 14:22:27 +0100

    Add attr_reader messages and initialized it with an empty array

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -1,18 +1,15 @@
 # frozen_string_literal: true
 
 class Proxy
-  attr_reader :proxy_object
+  attr_reader :proxy_object, :messages
 
   def initialize(proxy_object)
     @proxy_object = proxy_object
+    @messages = []
   end
 
   def method_missing(method_name, *args, &block)
+    messages << method_name
     proxy_object.send(method_name, *args, &block)
   end
-
-  def messages
-    [:upcase, :split]
-  end
-
 end
