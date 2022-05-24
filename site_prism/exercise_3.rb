9bf611f7f6c099f04b1c3e90501c150d33c8262e
Aiman Syed <12950084+AimanTirmizi@users.noreply.github.com>
2022-05-24 10:30:28 +0100

    Added test steps for Site Prism (#50)
    * Added test steps for the exercise 1
    
    * Added test steps for the exercise 2
    
    * Added test steps for the exercise 3

--- site_prism/exercise_3.rb
+++ site_prism/exercise_3.rb
@@ -5,24 +5,35 @@ require_relative 'setup'
 require_relative 'pages/dropdown'
 Setup.setup
 
-# Create and load the page
-page = Inputs.new
-page.load
+# Create and load the dropdown page
+dropdown = Dropdown.new
+dropdown.load
 
 # EXERCISE 3
 # We are going to simulate a user picking the last option from a dropdown menu
 
 # 1) Find the original dropdown with its label
+label = dropdown.dropdown_list
+puts label.text
 
 # 2) Click this and a bunch of new options should appear
+dropdown.dropdown_list.click
 
 # 3) Find all of the new dropdown options
+dropdown.dropdown_options
 
 # 4) Click on the last item from our list
+dropdown.dropdown_options.last.click
 
 # 5) Check we have done this correctly by finding out the label of the displayed option
+selected_option_label = dropdown.dropdown_options.last.text
+puts selected_option_label
 
 # 6) Compare the visible label to the item on the list we stored from the dropdown options
+selected_label = dropdown.dropdown_options.select do |option|
+  option['selected']
+end
+puts selected_label.first.text
 
 # Wait and close
 sleep 2
